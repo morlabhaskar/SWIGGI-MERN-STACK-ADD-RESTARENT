2f595dad634b2426a70a48e76a2086b0192400c0
morlabhaskar <morlabhaskar306@gmail.com>
2024-07-01 18:52:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from './vendorDashboard/pages/LandingPage'
+
+const App = () => {
+  return (
+    <>
+      <LandingPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/vendorDashboard/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import SideBar from '../components/SideBar';
+
+const LandingPage = () => {
+  return (
+    <>
+        <Navbar/>
+        <SideBar/>
+    </>
+      
+  )
+}
+
+export default LandingPage
