70e1fbcc009c9c956588052e4ea4d508a7e7b62e
morlabhaskar <morlabhaskar306@gmail.com>
2024-07-04 00:31:05 +0530

    first commit

--- src/vendorDashboard/components/forms/AddFirm.jsx
+++ src/vendorDashboard/components/forms/AddFirm.jsx
@@ -1,10 +1,25 @@
 // import { Form } from 'antd'
 import { Button, Input, Checkbox } from 'antd';
-import React from 'react'
+import React, { useState } from 'react'
 import { CgAddR } from 'react-icons/cg';
 import styled from 'styled-components'
 
 const AddFirm = () => {
+    const [firmName,setFirmName] = useState("");
+    const [area,setArea] = useState("");
+    const [category,setCategory] = useState([]);
+    const [region,setRegion] = useState([]);
+    const [offer,setOffer] = useState("");
+    const [file,setFile] = useState(null);
+
+    const handleFirmSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
     return (
         <Wrapper>
             <div className='add-firm p-5'>
@@ -12,36 +27,33 @@ const AddFirm = () => {
                 <hr className='w-[35px]' />
                 <div className="form border-2 border-orange-600 rounded-2xl h-[90%] mt-4 flex justify-center items-start">
                     <form className='flex flex-col gap-3 mt-4'>
-                        <Input type="text" placeholder='Firm Name' className='border-black' />
-                        <Input type="text" placeholder='Area' className='border-black' />
-                        {/* <Input type="text" placeholder='Category' className='border-black' /> */}
+                        <Input type="text" placeholder='Firm Name' className='border-black' name="firmName" />
+                        <Input type="text" placeholder='Area' className='border-black' name="area" />
                         <span>
-                            <h1 className="text-fuchsia-900 py-2">Category </h1>
+                            <h1 className="text-fuchsia-900 py-2">Category : </h1>
                             <span className="flex gap-7">
-                                <span>Veg <Checkbox /></span>
-                                <span>Non-Veg <Checkbox /></span>
+                                <span>Veg <Checkbox checked="" /></span>
+                                <span>Non-Veg <Checkbox checked="" /></span>
                             </span>
                         </span>
                         <span>
-                            <h1 className="text-fuchsia-900 py-2">Region </h1>
+                            <h1 className="text-fuchsia-900 py-2">Region :</h1>
                             <span className="flex gap-7">
-                                <span>South-Indian <Checkbox /></span>
-                                <span>North-Indian <Checkbox /></span>
+                                <span>South-Indian <Checkbox checked="" /></span>
+                                <span>North-Indian <Checkbox checked="" /></span>
                             </span>
                             <span className="flex gap-7 my-2">
-                                <span>Chineese <Checkbox /></span>
-                                <span>Bakery <Checkbox /></span>
+                                <span>Chineese <Checkbox checked="" /></span>
+                                <span>Bakery <Checkbox checked="" /></span>
                             </span>
                         </span>
-                        {/* <Input type="text" placeholder='Region' className='border-black' /> */}
-                        <Input type="text" placeholder='Offer' className='border-black' />
-                        <Input type="file" placeholder='Offer' className='border-black' />
-                        <Button className='rounded-none bg-orange-500 text-white'>Add</Button>
+                        <Input type="text" placeholder='Offer' className='border-black' name='offer' />
+                        <Input type="file" className='border-black' />
+                        <Button htmlType='submit' className='rounded-none bg-orange-500 text-white'>Add</Button>
                     </form>
                 </div>
             </div>
         </Wrapper>
-
     )
 }
 const Wrapper = styled.div`
